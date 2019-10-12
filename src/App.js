603e38290df34478358ab44596cdf3a7e8f20d6c
이건완 <psog03081@naver.com>
2019-10-12 21:03:06 +0900

    5강

--- src/App.js
+++ src/App.js
@@ -2,22 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  "name" : "이건완"
+const customers = [
+{
+  "id" : 1
+, "image" : "https://placeimg.com/64/64/1"   
+,  "name" : "이건완"
+, "birthday" : "920211"
+, "gender" : "남자"
+, "job" : "개발자"  
+},
+{
+  "id" : 2
+, "image" : "https://placeimg.com/64/64/2"   
+,  "name" : "손민규"
+, "birthday" : "930211"
+, "gender" : "남자"
+, "job" : "개발자"  
+},
+{
+  "id" : 3
+, "image" : "https://placeimg.com/64/64/3"   
+,  "name" : "이건완"
 , "birthday" : "920211"
 , "gender" : "남자"
 , "job" : "개발자"  
 }
+]
 // componenet web문에서에서 보여주기 위한 기본적인 단위
 class App extends Component {
   render(){
     return(
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />    
+      <div>
+        { customers.map( c =>{ return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)  }) }
+      </div>
     )
   }
 }
